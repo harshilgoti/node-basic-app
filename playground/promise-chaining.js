c592c8290d4229e5bf2cc4a1e55b77df11a877d5
harshilgoti <harshilgoti@gmail.com>
2021-12-09 20:59:02 +0530

    few change in promise-chaining,js

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const User = require("../src/models/users");
 
-User.findByIdAndUpdate("614dfd8406a69a080380209f", { age: 2 })
-  .then((user) => {
-    return User.countDocuments({ age: 1 });
-  })
+// User.findByIdAndUpdate("614dfd8406a69a080380209f", { age: 2 })
+//   .then((user) => {
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("614dfd8406a69a080380209f", 3)
   .then((result) => {
-    console.log(result);
+    console.log("result", result);
   })
   .catch((e) => {
-    console.log(e);
+    console.log("error", e);
   });
