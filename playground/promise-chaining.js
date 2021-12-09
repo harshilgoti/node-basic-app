6a9158247248cdcaf5e6745083a3324ec5b93a60
harshilgoti <harshilgoti@gmail.com>
2021-12-09 20:26:24 +0530

    few change

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,7 +1,7 @@
 require("../src/db/mongoose");
 const User = require("../src/models/users");
 
-User.findByIdAndUpdate("614dfd8406a69a080380209f", { age: 1 })
+User.findByIdAndUpdate("614dfd8406a69a080380209f", { age: 2 })
   .then((user) => {
     return User.countDocuments({ age: 1 });
   })
