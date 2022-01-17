b0c41ba231ec0bff857de414e622713280577a36
nikunjm-corelight <nikunjm@corelightsoftware.com>
2022-01-17 14:12:57 +0530

    added error handler

--- src/index.js
+++ src/index.js
@@ -44,9 +44,16 @@ const upload = multer({
   },
 });
 
-app.post("/upload", upload.single("upload"), (req, res) => {
-  res.send();
-});
+app.post(
+  "/upload",
+  upload.single("upload"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
