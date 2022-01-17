5a27ea71c3ecd195c8c8356a110e5d85c7c23503
nikunjm-corelight <nikunjm@corelightsoftware.com>
2022-01-17 15:24:05 +0530

    user avater add and delete

--- src/router/user.js
+++ src/router/user.js
@@ -186,4 +186,18 @@ router.delete("/users/me/avatar", auth, async (req, res) => {
   res.send();
 });
 
+router.get("/users/:id/avatar", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  try {
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/jpg");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
 module.exports = router;
